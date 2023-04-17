7329e8adcc60eb5f6f6029dffcec789b74bb3d0c
Neto student1 <teststudent1@neto.com>
2023-04-17 17:06:01 +0500

    Add Todos

---
+++ src/main/java/Epic.java
@@ -0,0 +1,23 @@
+public class Epic extends Task {
+    String[] subtasks = new String[0];
+
+    public Epic(int id, String[] subtasks) {
+        super(id);
+        this.subtasks = subtasks;
+    }
+
+    public String[] getSubtasks() {
+        return subtasks;
+    }
+
+    @Override
+    public boolean matches(String query) {
+        for (String task : subtasks)
+            if (task.contains(query)) {
+                return true;
+            }
+        return false;
+
+    }
+
+}
